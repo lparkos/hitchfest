a593f2d437ac5034e4f546228575a84861c6df72
Lauren <lauren.parkos@gmail.com>
2022-09-01 08:38:10 -0500

    updateing

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -37,7 +37,8 @@ const Schedule = () => {
         </div>
         <div className="col-6 pb-3">
           <div className="m1 ps-4"><p className="h5 white modesto">Welcome Drinks</p></div>
-          <div className="m1 ps-4"><p className="p5 white">Location TBD (Minneapolis)</p></div>
+          <div className="m1 ps-4"><p className="p5 white">Fulton Brewery / Garagery Room</p></div>
+          <div className="m1 ps-4"><p className="p5 white">Beer, Seltzers, Sliders</p></div>
         </div>
       </div>
 
